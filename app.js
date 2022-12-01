5b47fd5a0efdf08c8d1b17f6114001349028f46f
Flor Berardi <117835432+FlorBera@users.noreply.github.com>
2022-12-01 13:41:45 -0300

    Creando el proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const path = require("path")
+
+app.use(express.static("public")),
+
+app.listen (port, () => console.log("server listening on port", port));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/dh-home.html"))
+});
