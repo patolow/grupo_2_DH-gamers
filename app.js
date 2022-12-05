1f8e4e0b63f0099520b2471fc12efbe9c2b4844e
fabiotano <fabio.capriotti20@gmail.com>
2022-12-04 21:47:49 -0300

    Primer Draft Product details

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
-const path = require("path")
+const path = require("path");
 
-app.use(express.static("public")),
+app.use(express.static("public"));
 
 app.listen (port, () => console.log("server listening on port", port));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"))
-});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/views/index.html")));
+
+app.get("/product", (req, res) => res.sendFile(path.join(__dirname, '/views/productDetails.html')));
+
+
