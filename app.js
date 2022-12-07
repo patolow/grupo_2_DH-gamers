4ce9b75480b11b8d173f07db7d4d6e16665f197b
leanDichiara2022 <lean04_23@hotmail.com>
2022-12-07 19:08:21 -0300

    Primer Borrador de Login

--- app.js
+++ app.js
@@ -13,3 +13,4 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/views/index.html"
 app.get("/product", (req, res) => res.sendFile(path.join(__dirname, '/views/productDetails.html')));
 
 
+app.get("/registro", (req, res) => res.sendFile(path.join(__dirname, '/views/login.html')));
