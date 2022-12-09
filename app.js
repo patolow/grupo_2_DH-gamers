f94a2a14e37700ff5acb0c333b183b438a1a2464
FlorBera <florencia.berardi@hotmail.com>
2022-12-09 20:53:03 -0300

    Primer Registration Form

--- app.js
+++ app.js
@@ -12,4 +12,7 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/views/index.html"
 
 app.get("/product", (req, res) => res.sendFile(path.join(__dirname, '/views/productDetails.html')));
 
+app.get("/registration", (req, res) => res.sendFile(path.join(__dirname, '/views/registrationForm.html')));
+
+
 
