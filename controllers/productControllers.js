f4cf21229c60f7ca62b3adef1b28f55e5f90671a
patolow <patolow@hotmail.com>
2023-01-21 20:52:43 -0300

    sort products by category, header links

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -5,45 +5,94 @@ const fs = require("fs");
 const productsFilePath = path.join(__dirname, '../data/productsDataBasecopy.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf8"));
 
+const getImagesIndex = (category) => {
+  let imagenesindex = [];
+  for (let i = 0; i <= category.length - 1; i++) {
+    let firstImage = category[i].sliderImage.split(",")[0]
+    imagenesindex.push(firstImage);
+  }
+  return imagenesindex
+}
+
 const controller = {
 
-getProductDetail: (req, res)=> {
-    let idProduct = parseInt(req.params.id); 
-    let productDetail = products.find( product => product.id === idProduct)
-    let imagesSlider = productDetail.sliderImage.split(",") 
-    let imagesFooter= productDetail.footerImage.split(",")
-    res.render("productDetails", {productDetail, imagesSlider, imagesFooter})
-}, //done
+  getProductDetail: (req, res) => {
+    let idProduct = parseInt(req.params.id);
+    let productDetail = products.find(product => product.id === idProduct)
+    let imagesSlider = productDetail.sliderImage.split(",")
+    let imagesFooter = productDetail.footerImage.split(",")
+    res.render("productDetails", { productDetail, imagesSlider, imagesFooter })
+  }, //done
 
-getCreateProduct: (req, res)=> {
+  getCreateProduct: (req, res) => {
     res.render("createProduct")
-}, //to do
+  }, //to do
 
-createProduct: (req, res)=> {
-}, //to do
+  createProduct: (req, res) => {
+  }, //to do
 
-getEditProduct: (req, res)=> {
-    let idProduct = parseInt(req.params.id); 
-    let productToEdit = products.find( product => product.id === idProduct)
-    res.render("editProduct", {productToEdit})
-}, //done
+  getEditProduct: (req, res) => {
+    let idProduct = parseInt(req.params.id);
+    let productToEdit = products.find(product => product.id === idProduct)
 
-editProduct: (req, res)=> {
+    res.render("editProduct", { productToEdit })
+  }, //done
 
-}, //to do
+  editProduct: (req, res) => {
 
-getProductsList: (req, res)=> {
+  }, //to do
+
+  getProductsList: (req, res) => {
     let imagenesindex = [];
-    for (let i=0; i<= products.length-1; i++) {
-        let firstImage = products[i].sliderImage.split(",")[0]
-        imagenesindex.push(firstImage);
+    for (let i = 0; i <= products.length - 1; i++) {
+      let firstImage = products[i].sliderImage.split(",")[0]
+      imagenesindex.push(firstImage);
     }
-    res.render("productsList", {products, imagenesindex} )
-}, //done
+    res.render("productsList", { products, imagenesindex })
+  }, //done
 
+  //Methods for filtering products from productsList
 
-productCart: (req, res)=> {res.render("productCart")},
-destroy : (req, res) => {}
+  placasdevideo: (req, res) => {
+    let placasdevideo = products.filter(product => product.category === "Placas de video")
+    let imagenesindex = getImagesIndex(placasdevideo)
+    res.render("productsList", { products: placasdevideo, imagenesindex })
+  },
+
+  monitores: (req, res) => {
+    let monitores = products.filter(product => product.category === "Monitores")
+    let imagenesindex = getImagesIndex(monitores)
+    res.render("productsList", { products: monitores, imagenesindex })
+  },
+
+  microprocesadores: (req, res) => {
+    let microprocesadores = products.filter(product => product.category === "Microprocesadores")
+    let imagenesindex = getImagesIndex(microprocesadores)
+    res.render("productsList", { products: microprocesadores, imagenesindex })
+  },
+
+  motherboards: (req, res) => {
+    let motherboards = products.filter(product => product.category === "Motherboards")
+    let imagenesindex = getImagesIndex(motherboards)
+    res.render("productsList", { products: motherboards, imagenesindex })
+  },
+
+  watercooling: (req, res) => {
+    let watercooling = products.filter(product => product.category === "Water Cooling")
+    let imagenesindex = getImagesIndex(watercooling)
+    res.render("productsList", { products: watercooling, imagenesindex })
+  },
+
+  others: (req, res) => {
+    let others = products.filter(product => product.category === "Otros")
+    let imagenesindex = getImagesIndex(others)
+    res.render("productsList", { products: others, imagenesindex })
+  },
+
+  //Final filtering methods
+
+  productCart: (req, res) => { res.render("productCart") },
+  destroy: (req, res) => { },
 
 }
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const productControllers = require("../controllers/productControllers")
 const multer = require('multer');
-const storage = multer.diskStorage( {
-    destination: (req, file, cb) => cb(null, path.join(__dirname, '../../public/images/products')),
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, path.join(__dirname, '../../public/images/products')),
 
-    filename: (req, file, cb) => cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)),
+  filename: (req, file, cb) => cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)),
 });
 
-const upload = multer({storage})
+const upload = multer({ storage })
 
 
 router.get("/detail/:id/", productControllers.getProductDetail); //done
@@ -22,8 +22,14 @@ router.post("/create/", productControllers.createProduct); //to do
 
 router.get("/list/", productControllers.getProductsList); //done
 
+router.get("/list/placasdevideo/", productControllers.placasdevideo);
+router.get("/list/monitores/", productControllers.monitores);
+router.get("/list/microprocesadores/", productControllers.microprocesadores);
+router.get("/list/motherboards/", productControllers.motherboards);
+router.get("/list/watercooling/", productControllers.watercooling);
+router.get("/list/others/", productControllers.others);
 
-router.get("/cart/", productControllers.productCart); 
+router.get("/cart/", productControllers.productCart);
 
 router.delete('/delete/:id/', productControllers.destroy);
 
