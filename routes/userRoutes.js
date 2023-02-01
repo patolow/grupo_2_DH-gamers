ca8d3f00598869d7f90015de5ef72864baaa3b78
CristianDiazDev <Cristianrdiaz@hotmail.com.ar>
2023-02-01 16:52:47 -0300

    Validación de email ya existente

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,8 @@ const multer = require('multer');
 const { body } = require('express-validator')
 const guestMiddleware = require('../middlewares/guestMiddleware')
 const authMiddleware = require('../middlewares/authMiddleware')
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf8"));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, path.join(__dirname, '../public/images/users')),
@@ -20,9 +22,16 @@ const registerValidations = [
   body('fullName').notEmpty().withMessage('Debes escribir un nombre'),
   body('userName').notEmpty().withMessage('Debes escribir un nombre de usuario'),
   body('birthday').notEmpty().withMessage('Debes seleccionar tu fecha de nacimiento'),
-  body('email')
+  body('email').custom((value, { req }) => {
+    for(let i = 0; i < users.length; i++ ){
+    let mailAChequear = req.body.email;
+      if (mailAChequear === users[i].email) {
+      throw new Error('Ese mail ya está en uso');
+    }
+    }return true})
     .notEmpty().withMessage('Debes escribir tu correo electrónico').bail()
     .isEmail().withMessage('Debes escribir un formato de correo electrónico válido'),
+    
   body('password').notEmpty().withMessage('Debes escribir una contraseña'),
   body('confirmPassword').notEmpty().withMessage('Debes repetir tu contraseña'),
   body('termsAndConditions').custom((value, {req}) => {
