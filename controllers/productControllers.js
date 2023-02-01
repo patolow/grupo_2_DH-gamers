87cac3198f63ac9809d3ef68dacec03fba56521d
Jerecastillo25 <jerecastillo25@gmail.com>
2023-02-01 18:34:40 -0300

    fixing productsAll

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -124,11 +124,11 @@ const controller = {
   },
 
   productsAll: (req, res) => {
-   let imagenes =  []
-   for (let index = 0; index < products.length; index++) {
-    let algo = array[index];
-    imagenes = algo
-   }
+    let imagenesindex = [];
+    for (let i = 0; i <= products.length - 1; i++) {
+      let firstImage = products[i].sliderImage.split(",")[0]
+      products[i].sliderImage = firstImage
+    }
     res.render("productsAll", { products })
   },
 
