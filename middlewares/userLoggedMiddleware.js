8d2e78de82ed14646a70b7fdaf4350bdf40f1551
patolow <patolow@hotmail.com>
2023-02-04 10:17:47 -0300

    updated header

--- middlewares/userLoggedMiddleware.js
+++ middlewares/userLoggedMiddleware.js
@@ -3,6 +3,7 @@ function userLoggedMiddleware (req,res,next){
 
     if (req.session && req.session.usuarioLogueado) {
         res.locals.isLogged = true;
+        res.locals.usuarioLogueado = req.session.usuarioLogueado
     }
 
     next();
