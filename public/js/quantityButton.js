b2233873926307f0a76ae69e7e30a4acbb7a6c6f
fabiotano <fabio.capriotti20@gmail.com>
2023-03-24 18:12:45 -0300

    JS para boton stock

---
+++ public/js/quantityButton.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', function (){
+
+    const disminuirBtn = document.querySelector('.qty-minus'); 
+    const aumentarBtn = document.querySelector('.qty-plus');
+    const cantidad = document.querySelector('.quantity');
+    
+    // Agregar evento de clic al botón "-"
+    disminuirBtn.addEventListener('click', function (){
+        // Obtener el valor actual del campo de cantidad
+        let cantidadActual = parseInt(cantidad.value);
+  
+        // Disminuir la cantidad en 1 si el valor actual es mayor que 0
+        if (cantidadActual > 0) {
+            cantidadActual--;
+        }
+
+        // Actualizar el valor del campo de cantidad
+        cantidad.value = cantidadActual.toString();
+});
+
+    // Agregar evento de clic al botón "+"
+    aumentarBtn.addEventListener('click', function () {
+
+        let cantidadActual = parseInt(cantidad.value);
+        cantidadActual++;
+        cantidad.value = cantidadActual.toString();
+    })
+
+})
