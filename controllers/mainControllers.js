553a39c7632bab70e72ae8beccbe924dc6d3a789
fabiotano <fabio.capriotti20@gmail.com>
2023-04-09 13:10:58 -0300

    logica maincontrollers cambiada por DB

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -3,11 +3,6 @@ const path = require("path");
 const fs = require('fs');
 const db = require("../database/models");
 
-// const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-// const products = JSON.parse(fs.readFileSync(productsFilePath, "utf8"));
-
-
-
 const controlador = {
 
 index: (req, res)=> {
@@ -21,10 +16,12 @@ index: (req, res)=> {
         for (let i=0; i<= bestSeller.length-1; i++) {
         let firstImage = bestSeller[i].sliderImage.split(",")[0]
         bestSeller[i].sliderImage = firstImage
-    }
+        }
     // console.log(bestSeller)
     res.render("index", {bestSeller})
     })
+
+    .catch(err => console.error(err))
 
     }
 
