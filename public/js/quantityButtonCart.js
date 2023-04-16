6c5a9a241ae67a18c8ac87c8d303d9c7d60539ae
patolow <patolow@hotmail.com>
2023-04-16 14:23:33 -0300

    qty btns cart working

--- public/js/quantityButtonCart.js
+++ public/js/quantityButtonCart.js
@@ -1,73 +1,71 @@
 window.addEventListener('load', function (){
 
-    const disminuirBtn = document.querySelector('.qty-minus'); 
-    const aumentarBtn = document.querySelector('.qty-plus');
-    const cantidad = document.querySelector('.quantity');
-    
+  const disminuirBtns = document.querySelectorAll('.qty-minus'); 
+  const aumentarBtns = document.querySelectorAll('.qty-plus');
+  const cantidades = document.querySelectorAll('.quantity');
+
+  disminuirBtns.forEach(function(disminuirBtn, index) {
     // Agregar evento de clic al botón "-"
     disminuirBtn.addEventListener('click', function (){
-        // Obtener el valor actual del campo de cantidad
-        let cantidadActual = parseInt(cantidad.value);
-  
-        // Disminuir la cantidad en 1 si el valor actual es mayor que 0
-        if (cantidadActual > 1) {
-            cantidadActual--;
-            let productId = document.querySelector('[name="productId"]').value;
-            let productoARemover = {
-                productId: parseInt(productId)
-            }
-            // console.log(productoARemover);
-            axios.post('/cart/remove', productoARemover)
-            Swal.fire({
-                position: 'top-center',
-                icon: 'success',
-                title: `1 item removido! Ahora quedan ${cantidadActual}`,
-                showConfirmButton: false,
-                timer: 1700
-              })
+      // Obtener el valor actual del campo de cantidad
+      let cantidadActual = parseInt(cantidades[index].value);
+
+      // Disminuir la cantidad en 1 si el valor actual es mayor que 0
+      if (cantidadActual > 1) {
+        cantidadActual--;
+        let productId = document.querySelector('[name="productId"]').value;
+        let productoARemover = {
+            productId: parseInt(productId)
         }
-        
-        // Actualizar el valor del campo de cantidad
-        cantidad.value = cantidadActual.toString()
+        axios.post('/cart/remove', productoARemover)
+        Swal.fire({
+          position: 'top-center',
+          icon: 'success',
+          title: `1 item removido! Ahora quedan ${cantidadActual}`,
+          showConfirmButton: false,
+          timer: 1700
+        })
+      }
 
-
+      // Actualizar el valor del campo de cantidad
+      cantidades[index].value = cantidadActual.toString()
     });
+  });
 
-    // // Agregar evento de clic al botón "+"
+  aumentarBtns.forEach(function(aumentarBtn, index) {
+    // Agregar evento de clic al botón "+"
     aumentarBtn.addEventListener('click', function () {
 
-        let cantidadActual = parseInt(cantidad.value);
-        let productStock = document.querySelector('[name="productStock"]').value;
-        let productId = document.querySelector('[name="productId"]').value;
+      let cantidadActual = parseInt(cantidades[index].value);
+      let productStock = document.querySelectorAll('[name="productStock"]')[index].value;
+      let productId = document.querySelectorAll('[name="productId"]')[index].value;
 
 
-        if (cantidadActual < productStock) {
-            cantidadActual++;
-            cantidad.value = cantidadActual.toString();
+      if (cantidadActual < productStock) {
+        cantidadActual++;
+        cantidades[index].value = cantidadActual.toString();
 
-            const productoAgregar = {
-                productId: parseInt(productId)
-            }
-            // console.log(productoAgregar);
-            axios.post('/cart/add', productoAgregar)
-            Swal.fire({
-                position: 'top-center',
-                icon: 'success',
-                title: `1 item agregado! Ahora son ${cantidadActual}`,
-                showConfirmButton: false,
-                timer: 1700
-              })
+        const productoAgregar = {
+          productId: parseInt(productId)
         }
-        else {
-            Swal.fire({
-                position: 'top-center',
-                icon: 'success',
-                title: `No se pueden agregar  mas item, el stock maximo de ${cantidadActual} alcanzado`,
-                showConfirmButton: false,
-                timer: 1500
-            })
-        }
-        
+        axios.post('/cart/add', productoAgregar)
+        Swal.fire({
+          position: 'top-center',
+          icon: 'success',
+          title: `1 item agregado! Ahora son ${cantidadActual}`,
+          showConfirmButton: false,
+          timer: 1700
+        })
+      } else {
+        Swal.fire({
+          position: 'top-center',
+          icon: 'success',
+          title: `No se pueden agregar  mas item, el stock maximo de ${cantidadActual} alcanzado`,
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
     });
+  });
 
-})
+});
