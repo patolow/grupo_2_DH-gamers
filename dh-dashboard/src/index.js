929fc8acca730553646bbff4df1ec48b8b796296
CristianDiazDev <Cristianrdiaz@hotmail.com.ar>
2023-04-19 18:31:06 -0300

    avances en el sprint (dashboard)

--- dh-dashboard/src/index.js
+++ dh-dashboard/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles.css';
+import './styles-dashboard.css';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
