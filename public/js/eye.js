5d89fff1d8dc1bfcc3cfdf8a33196778a0f04433
Patricia Sol Löwenstein <117777055+patolow@users.noreply.github.com>
2023-04-28 16:29:41 +0000

    eye registration form working

--- public/js/eye.js
+++ public/js/eye.js
@@ -12,3 +12,18 @@ eye.addEventListener("click", function() {
     }
 
 })
+
+let eye2 = document.getElementById("eye2")
+let input2 = document.getElementById("formulario__input2")
+
+eye2.addEventListener("click", function() {
+
+    if (input2.type == "password" && input2.value) {
+        input2.type  = "text"
+        eye2.style.opacity = 0.2
+    } else {
+        input2.type  = "password"
+        eye2.style.opacity = 0.8
+    }
+
+})
